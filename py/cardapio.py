6ff0c269140f1900c5cc0535f9426d02ca8b098c
Jaqueline Silva <jg76082365@gmail.com>
2024-08-02 14:31:16 -0300

    modificando o arquivo cardapio.py

--- py/cardapio.py
+++ py/cardapio.py
@@ -1,1 +1,22 @@
 from xml.dom.minidom import parse
+ 
+dom = parse ("xml/cardapio.xml")
+
+cardapio = dom.documentElement
+
+pratos = cardapio.getElementsByTagName('prato')
+
+id_prato = 0
+
+for prato in pratos:
+    id_prato += 1
+    id_prato_attr = prato.getAttribute('id')
+    nome_elemento = prato.getElementsByTagName('nome')[0]
+    nome = nome_elemento.firstChild.nodeValue
+    print(f"{id_prato} - {id_prato_attr}: {nome}")
+
+id_pergunta = int(input("Digite o número do prato para mais informações: "))
+prato = pratos[id_pergunta-1]
+print("---\n")
+
+id_prato_attr = prato.getAttribute('id')

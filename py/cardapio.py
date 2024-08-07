7a421157b7c9304d0672447d7155e95b514de334
Jaqueline Silva <jg76082365@gmail.com>
2024-08-07 17:29:34 -0300

    adionanco cardapio.py

--- py/cardapio.py
+++ py/cardapio.py
@@ -15,8 +15,35 @@ for prato in pratos:
     nome = nome_elemento.firstChild.nodeValue
     print(f"{id_prato} - {id_prato_attr}: {nome}")
 
-id_pergunta = int(input("Digite o número do prato para mais informações: "))
-prato = pratos[id_pergunta-1]
-print("---\n")
+id_pergunta = input("Digite o número do prato para mais informações: ")
 
-id_prato_attr = prato.getAttribute('id')
+
+for prato in pratos:
+    id_prato_attr = prato.getAttribute('id')
+    if id_pergunta == id_prato_attr:
+        nome = prato.getElementsByTagName("nome")[0].firstChild.nodeValue
+        descricao = prato.getElementsByTagName("descricao")[0].firstChild.nodeValue
+        ingredientes = prato.getElementsByTagName('ingrediente')
+        preco = prato.getElementsByTagName('preco')[0].firstChild.nodeValue
+        moeda = prato.getElementsByTagName('preco')[0].getAttribute('moeda')
+        calorias = prato.getElementsByTagName('calorias')[0].firstChild.nodeValue
+        tempo_preparo = prato.getElementsByTagName('tempoPreparo')[0].firstChild.nodeValue
+
+
+        print(f"ID: {id_prato}")
+        print(f"Nome: {nome}")
+        print(f"Descrição: {descricao}")
+        for ingrediente in ingredientes:
+            ingrediente_value = ingrediente.firstChild.nodeValue
+            print(f" {ingrediente_value}")
+        print(f"Preço: {preco} ({moeda})")
+        print(f"Calorias: {calorias}")
+        print(f"Tempo de Preparo: {tempo_preparo}")
+        break
+
+else:
+    print("Esse prato nao existe.")
+
+
+
+

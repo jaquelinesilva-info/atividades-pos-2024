9c2022408ae15972a6936d80d78b7e3baad791c1
jaqueline silva <jg76082365@gmail.com>
2024-09-05 10:58:23 -0300

    adicionando  a autenicaçao

--- clients/boletim_suap.py
+++ clients/boletim_suap.py
@@ -1,7 +1,21 @@
 import requests
-from getpass import getpass
 from tabulate import tabulate
 
-# URL base da API do SUAP
-api_url = "https://suap.ifrn.edu.br/api/"
+
+username = input("Digite sua Matricula: ")
+password = input("Digite sua Senha: ") 
+
+auth_url = "https://suap.ifrn.edu.br/api/v2/autenticacao/token/"
+
+
+response = requests.post(auth_url, data={"username": username, "password": password})
+
+if response.status_code == 200:
+    token_data = response.json()
+    api_key = token_data.get("access")
+    refresh_token = token_data.get("refresh")
+    print("Token obtido com sucesso.")
+else:
+    print("Erro ao obter o token:", response.status_code, response.text)
+    exit(1)
 
